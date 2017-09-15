89ba2873d08a1e20e36d4c1b19e87127c2cec510
manipoospo <30549810+manipoospo@users.noreply.github.com>
2017-09-15 09:38:03 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int b,e,r;
+printf("enter the base number :");
+scanf("%d",&b);
+printf("Enter the exponent number :");
+scanf("%d",&e);
+r=pow(b,e);
+printf(" result is :%d\n",r);
+}
